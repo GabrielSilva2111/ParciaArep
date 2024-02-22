0fb96e98c1e5bd97f4d65652b59528c0396d58a8
unknown <gabriel.silva@labinfo.is.escuelaing.edu.co>
2024-02-22 13:55:11 -0500

    clases descargadas con importaciones

---
+++ src/main/java/com/mycompany/parcialarep/ParcialArep.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcialarep;
+
+/**
+ *
+ * @author gabriel.silva
+ */
+public class ParcialArep {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
